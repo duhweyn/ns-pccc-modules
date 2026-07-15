8d29359ec27d876af688ee421f656d97869dfeb5
duhweyn <dwaynebaste0106@gmail.com>
2026-07-15 14:37:07 +0800

    Fix module manifest format (config.xml instead of module.json)

---
+++ NsPcccDashboardFix/NsPcccDashboardFixModule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\NsPcccDashboardFix;
+
+use App\Services\Module;
+
+class NsPcccDashboardFixModule extends Module
+{
+    public function __construct()
+    {
+        parent::__construct( __FILE__ );
+    }
+}
---
+++ NsPcccTheme/NsPcccThemeModule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\NsPcccTheme;
+
+use App\Services\Module;
+
+class NsPcccThemeModule extends Module
+{
+    public function __construct()
+    {
+        parent::__construct( __FILE__ );
+    }
+}
